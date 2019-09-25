dc550b6bcf29aa628d94068e9bfcd31afff2ec38
Teemu Takaluoma <teemu.takaluoma@arm.com>
2019-09-25 10:44:26 +0300

    factory-configurator-client-example 4.0.0

--- linux-psa-config.cmake
+++ linux-psa-config.cmake
@@ -4,6 +4,7 @@ include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../)
 SET(SA_PV_OS_NAME "linux")
 SET(CMAKE_C_FLAGS "-std=c99")
 OPTION(USE_CRYPTO_SUBMODULE "Build and use libmbedcrypto from the crypto submodule." ON)
+OPTION(LINK_WITH_TRUSTED_STORAGE "Explicitly link mbed TLS library to trusted_storage." ON)
 
 SET(PAL_TLS_BSP_DIR ${NEW_CMAKE_SOURCE_DIR}/mbed-cloud-client/mbed-client-pal/Configs/mbedTLS)
 
@@ -13,8 +14,8 @@ add_definitions(
     -DPAL_USER_DEFINED_CONFIGURATION="\\"source/pal_config_linux.h"\\"
     -DMBED_CLIENT_USER_CONFIG_FILE="\\"source/mbed_cloud_client_user_config.h"\\"
     -DMBED_CLOUD_CLIENT_USER_CONFIG_FILE="\\"source/mbed_cloud_client_user_config.h"\\"
+    -DPSA_STORAGE_USER_CONFIG_FILE="\\"source/psa_storage_user_config.h"\\"
     -DFCE_ETHERNET_INTERFACE
-    -DCRYPTO_STORAGE_FILE_LOCATION="\\"pal/BACKUP/"\\"
     -DMBED_CONF_MBED_CLOUD_CLIENT_PSA_SUPPORT
 )
 
@@ -23,7 +24,4 @@ add_definitions(
     -DMBEDTLS_USE_PSA_CRYPTO
     -DMBEDTLS_PSA_CRYPTO_C
     -DMBEDTLS_PSA_CRYPTO_STORAGE_C
-    -DMBEDTLS_PSA_CRYPTO_STORAGE_FILE_C
-    -DMBEDTLS_FS_IO
-    -DMBEDTLS_PSA_ITS_FILE_C
 )
